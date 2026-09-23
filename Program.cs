172952e00532dc9ae0d929a1e76f22f0d48bd5e9
Marlon Labas <marlon_lab@hotmail.com>
2026-09-23 16:24:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapOpenApi();
+
+app.UseSwaggerUI (op =>
+{
+    op.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
+
+app.UseHttpsRedirection();
+
+app.Run();
